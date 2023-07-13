c3cab79c3d793e1eed187a90aa54867ae733b427
nchimunya-joseph <nchimunya.backlogcode@gmail.com>
2023-07-13 13:24:46 +0200

    host change

--- app.py
+++ app.py
@@ -6,9 +6,9 @@ app = Flask(__name__)
 def index():
     return render_template("index.html")
 
-@app.route("/auction")
+@app.route("/auctions")
 def auction():
     return render_template("auctions.html")
 
 if __name__ == "__main__":
-    app.run(debug=True, port=7000)
+    app.run(debug=True, host="0.0.0.0")
